e90034b9b8b97cbb18ec61985683f1d81491b929
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2020-09-27 15:50:09 +0300

    Add base project

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=.\mirceaweatherappbot-Web-Deploy.pubxml -p:Password=p0pKbvr6fCiXnvYKNBgm4bo3bw7oJo34AFFc63vzePQiJefDHoD6TPb6dD9k
+
